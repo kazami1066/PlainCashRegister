d693124df49ecb8de333d27e762f640a1c468800
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-20 21:36:27 +0900

    Javascriptリストの作成

---
+++ app/javascript/lists.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+
+const pullDownButton = document.getElementById("setting-logo")
+const pullDownParents = document.getElementById("pull-down")
+
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:lightgrey;")
+    pullDownParents.setAttribute("style", "display:block;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    this.removeAttribute("style", "background-color:lightgrey;")
+    pullDownParents.removeAttribute("style", "display:block;")
+  })
+
+})
