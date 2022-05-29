814ca1ca4bb749bf730be9c325aee23156e02e7a
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-29 15:26:18 +0900

    商品追加機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   validates :price,           presence:true
   validates :sales_start_date
   validates :sales_end_date
+  validates :category_id,     presence:true
 end
