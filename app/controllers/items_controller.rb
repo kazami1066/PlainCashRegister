3887f8770bc2999d562bab5cc323ddea886d45a2
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-29 15:26:29 +0900

    商品削除追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,14 +11,21 @@ class ItemsController < ApplicationController
     @category = Category.new
     @item = Item.new(item_params) #データを新規登録するためのインスタンス生成
     if @item.save #データをデータベースに保存するためのsaveメソッド実行
+      binding.pry
       redirect_to root_path #トップ画面へリダイレクト
     else 
       render :new
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params #ストロングパラメータ
-    params.require(:item).permit(:item_name, :price).merge(category_id: @category.id) #パラメーターのキー
+    params.require(:item).permit(:item_name, :price, :category_id)#パラメーターのキー
   end
 end
