ea8091bf79c8c863834bedd6854bb8b180e130a1
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-06-01 20:51:23 +0900

    買い物カゴに商品追加実装

--- app/javascript/order.js
+++ app/javascript/order.js
@@ -1,5 +1,24 @@
 // window.addEventListener('load', function(){
 
-//   const pullDownButton = document.getElementById("menu")
-//   console.log(pullDownButton)
+//   // const pullDownButton = document.getElementById("menu")
+//   // console.log(pullDownButton)
+//   ('div.itemName').each(function(i){
+//     (this).attr('id','item' + (i+1));
+//   });
+
+//   ('div.itemPrice').each(function(i){
+//     (this).attr('id','price' + (i+1));
+//   });
+
+  
 // })
+
+(function(){
+  ('div.itemName').each(function(i){
+    (this).attr('id','item' + (i+1));
+  });
+
+  ('div.itemPrice').each(function(i){
+    (this).attr('id','price' + (i+1));
+  });
+});
--- app/javascript/totalprice.js
+++ app/javascript/totalprice.js
@@ -14,25 +14,18 @@ window.addEventListener('load', function(){
     });
     // listItemから降りると色が戻るコード ここまで
     // listItemをクリックするとlistクラスに移動する ここから
-
-    // target.addEventListener('click', function() {
-      // for (var i=0; i < menuList.length; i++) {
-      //   menuList[i].addEventListener('click', function() {
-      //   // thisはli[i]にあたる
-      //   this.append(addList);
-      //   });
-      // };
-
     target.addEventListener('click', function() {
       var boxes = document.getElementById('list');
-      var items = document.getElementById('item');
-      var prices = document.getElementById('price');
+      // var items = document.getElementById('item');
+      // var prices = document.getElementById('price');
       var clone = boxes.firstElementChild.cloneNode(true);
-      var cloneItem = items.firstChild.cloneNode(true);
-      var clonePrice = prices.firstChild.cloneNode(true);
-      clone.appendChild(cloneItem);
-      clone.appendChild(clonePrice);
-      boxes.appendChild(clone);
+      // var cloneItem = items.firstChild.cloneNode(true);
+      // var clonePrice = prices.firstChild.cloneNode(true);
+      var clones = target.cloneNode(true);
+      // clone.appendChild(cloneItem);
+      // clone.appendChild(clonePrice);
+      boxes.appendChild(clones);
+      // boxes.appendChild(clone);
       clone.classList.add("item");
     });
   });
