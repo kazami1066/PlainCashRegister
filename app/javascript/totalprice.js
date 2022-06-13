ac4d0d633303db42e0d1bef37d86e6d8f3db9cab
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-06-13 14:17:51 +0900

    不要なコードの削除

--- app/javascript/totalprice.js
+++ app/javascript/totalprice.js
@@ -16,23 +16,16 @@ window.addEventListener('load', function(){
     // listItemをクリックするとlistクラスに移動する ここから
     target.addEventListener('click', function() {
       var boxes = document.getElementById('list');
-      // var items = document.getElementById('item');
-      // var prices = document.getElementById('price');
-      // var clone = boxes.firstElementChild.cloneNode(true);
-      // var cloneItem = items.firstChild.cloneNode(true);
-      // var clonePrice = prices.firstChild.cloneNode(true);
       var clones = target.cloneNode(true);
-      // clone.appendChild(cloneItem);
-      // clone.appendChild(clonePrice);
       boxes.appendChild(clones);
-      // boxes.appendChild(clone);
       clones.classList.add("item");
 
     var div_children = document.querySelectorAll('.listItem.item #price');
       div_children.forEach(function(targets) {
         targets.classList.add('prices');
       });
-      
+    // listItemをクリックするとlistクラスに移動する ここまで
+    // 合計金額を自動計算する  ここから
     var totalPriceCash = document.getElementById("total_price");
     var total = 0 ;
     var prices = document.getElementsByClassName('prices');
@@ -45,43 +38,7 @@ window.addEventListener('load', function(){
       var addTaxDom = document.getElementById("add-tax-price");
         addTaxDom.innerHTML = Math.round(total / 1.1 * 0.1 ).toLocaleString();
     }
-      
-    // let total = prices.reduce(function(sum, element){
-    //   return sum + element.prices;
-    // }, 0);
-
-    // console.log(prices.innerHTML);
-    
-    // function keisan(){
-    //   goukei=0;
-    //   for(i=0;i<=2;i++){
-    //   if(document.myform.elements[i].checked==true){
-    //   goukei=goukei+plice[i];
-      // }
-    //   }
-
-      // prices.forEach(function(totalPrice) {
-      // totalPriceCash.innerHTML = totalPrice.innerHTML
-
-
-      // listItemをクリックするとlistクラスに移動する ここまで
-
-      // 合計金額を自動計算する  ここから
-      
-      // var clonesPrices = document.getElementById("prices")
-      // var totalPriceCash = document.getElementById("total_price")
-      // totalPriceCash = Math.round(clonesPrices);
-
-
-      // const numbers = ('prices');
-      // let total = 0;
-
-      // for (let i = 0; i < numbers.length; i++) {
-      //   total += numbers[i];
-      // }
-      // console.log(total);
-            // 合計金額を自動計算する  ここまで
+    // 合計金額を自動計算する  ここまで
       });
     });
   });
-// });
