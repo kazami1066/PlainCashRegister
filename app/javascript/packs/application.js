0a5ee66badb73c26e211486acb29a11d9cf1cc25
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-06-17 22:23:11 +0900

    キーワード検索機能の実装（未）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 require("../lists")
 require("../order")
 require("../totalprice")
-
+require("../search")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/search.js
@@ -0,0 +1,17 @@
+// function clickSearch(){
+//   var search_catch = document.getElementById("search_bar").value;
+//   console.log(search_catch);
+// }
+
+window.addEventListener('load', function(){
+
+  var menuList = document.getElementsByClassName("menu");
+  // var children = menuList.children;
+  console.log(menuList.children);
+    // for (i = 0; i < itemName.length; i++) { 
+    // var itemNames = itemName[i];
+    // // for (i = 0; i < itemNames.length; i++) { 
+    // console.log(itemNames);
+  });
+// });
+
